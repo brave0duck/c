2bb9e53c958a149ea6986ae2e60f98a248822c5b
brave0duck <brave0duck@naver.com>
2026-02-03 18:30:16 +0900

    leetcode - two sum. using stack

--- leetcode/02-twosum.c
+++ leetcode/02-twosum.c
@@ -5,7 +5,7 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#define _MAX_ 100
+#include <math.h>
 
 typedef struct n{
     int data;
@@ -19,6 +19,7 @@ typedef struct ll{
 
 typedef struct stack{
     int ptr;
+    int max;
     int * pstack;
 }Stk;
 
@@ -27,42 +28,95 @@ int Init_Stack(Stk *s, int n){
     if(s->pstack == NULL){
         return -1;
     }
+    s->max = n;
     s->ptr=0;
 }
 int Push_Stack(Stk *s,int data){
-    if(s->ptr >= _MAX_){
+    if(s->ptr > s->max){
         return -1;
     }
-    s->pstack[s->ptr++];
+    s->pstack[s->ptr++] = data;
 }
-int Pop_Stack(Stk *s,int *data){
-    if(s->ptr >= 0){
+int Pop_Stack(Stk *s){
+    if(s->ptr <= 0){
         return -1;
     }
-    *data = s->pstack[s->ptr];
-    s->ptr--;
+    return s->pstack[--s->ptr];
+}
+int Terminate(Stk *s){
+    if(s->pstack != NULL){
+        free(s->pstack);
+    }
+    s->max=0;
+    s->ptr;
+}
+int numbers_of_digits(int n){
+    if(n<0)
+        return -1;
+    int result=0;
+    int count=1;
+    while(1){
+        if( 1 > (n / pow(10,count))){
+            return count;
+        }
+        count++;
+    }
 }
 int main(void){
-    int one_size, two_size;
+    int result=0;    int data=0;
+    int * final_arr;
+    int nDigit;
+
+
     int l1[] = {2,4,3};
     int l2[] = {5,6,4};
-
-    Stk *s1, *s2 ;
-    InitStack(s1,_MAX_);
-    InitStack(s2,_MAX_);
-
-    for(int i=0; i<sizeof(l1); i++){
-        Push_Stack(s1,l1[i]);
+    int one_size = sizeof(l1)/sizeof(l1[0]);
+    int two_size = sizeof(l2)/sizeof(l2[0]);
+    int total_size = one_size > two_size ? one_size: two_size;
+    
+    Stk s1, s2, s3 ;
+    Init_Stack(&s1,one_size);
+    Init_Stack(&s2,two_size);
+    // 1
+    for(int i=0; i<one_size ; i++){
+        Push_Stack(&s1,l1[i]);
     }
-    for(int j=0; j<sizeof(l2); j++){
-        Push_Stack(s2,l2[i]);
+    for(int i=0; i<two_size ; i++){
+        Push_Stack(&s2,l2[i]);
     }
-    one_size = s1->ptr+1;
-    two_size = s2->ptr+1;
+    // 2
+    while( total_size){
+        int number=0;
+        if (total_size == s1.ptr){
+            data = Pop_Stack(&s1);
+            if( data != -1)
+                number += data;
+        }
+        if(total_size == s2.ptr){
+            data = Pop_Stack(&s2);
+            if( data != -1)
+                number += data;
+        }
+        number *= pow(10,(total_size-1));
 
-    while( one_size - two_size > 0 ? one_size-- : two_size--){
-        int 
+        result += number;
+        total_size--;
     }
+
+    nDigit = numbers_of_digits(result);
+    final_arr = (int *)calloc(nDigit,sizeof(int));
+    for(int i=0; i< nDigit; i++){
+        int r = result % (int)(pow(10,1));
+        final_arr[i] = r;
+        result /= (int)(pow(10,1));
+    }
+    for(int i=0; i<nDigit ; i++){
+        printf("number  is %d\n", final_arr[i]);
+    }
+    
+    free(final_arr);
+    Terminate(&s1);
+    Terminate(&s2);
 
     return 0;
 }
